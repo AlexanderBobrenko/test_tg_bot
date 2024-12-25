d5a8ac5e871cc7b4b4551d266ed38ca49afe9299
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-25 17:53:22 +0300

    Добавлена обработка команды /help в main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from config import TOKEN
-from handlers.commands import start_command, login_command
+from handlers.commands import start_command, login_command, help_command
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 dp.message.register(start_command, Command(commands=['start']))
 dp.message.register(login_command, Command(commands=['login']))
+dp.message.register(help_command, Command(commands=['help']))
 
 async def main():
     try:
