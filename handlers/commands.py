2bf74ea378698e4fa8daec1312cdf600cbc82f42
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-25 17:50:24 +0300

    Добавлена обработка команды /help в ./handlers/commands.py

--- handlers/commands.py
+++ handlers/commands.py
@@ -12,3 +12,12 @@ async def login_command(message: types.Message):
     await message.answer(
         "Вам необходимо авторизоваться через GitHub, Яндекс ID или через код."
     )
+
+#/help
+async def help_command(message: types.Message):
+    help_text = """
+<b><i>Доступные команды:</i></b>
+<i>/start</i> - Начать работу с ботом
+<i>/help</i> - Получить список команд
+"""
+    await message.answer(help_text, parse_mode="HTML")
