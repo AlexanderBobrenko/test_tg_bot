e4fe3cd5b0a91c6863d14d9fd6dae6383edf1a6b
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-27 19:29:51 +0300

    fixed bugs

--- handlers/keyboards.py
+++ handlers/keyboards.py
@@ -1,8 +1,13 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_keyboard():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    kb.add(KeyboardButton('Авторизация'))
-    kb.add(KeyboardButton('Тесты'))
-    kb.add(KeyboardButton('Помощь'))
+    kb = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Авторизация")],
+            [KeyboardButton(text="Тесты")],
+            [KeyboardButton(text="Помощь")]
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=False
+    )
     return kb
