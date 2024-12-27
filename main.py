c1d246212edfd0cc1feb1d3d6eaa567736b2a091
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-27 19:29:51 +0300

    Добавлена обработка кнопок клавиатуры в main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from config import TOKEN
-from handlers.commands import start_command, login_command, help_command
+from handlers.commands import start_command, login_command, help_command, auth_button, tests_button, help_button_command
+from handlers.keyboards import get_main_keyboard
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -10,6 +11,10 @@ dp = Dispatcher()
 dp.message.register(start_command, Command(commands=['start']))
 dp.message.register(login_command, Command(commands=['login']))
 dp.message.register(help_command, Command(commands=['help']))
+
+dp.message.register(auth_button, lambda message: message.text == 'Авторизация')
+dp.message.register(tests_button, lambda message: message.text == 'Тесты')
+dp.message.register(help_button_command, lambda message: message.text == 'Помощь')
 
 async def main():
     try:
