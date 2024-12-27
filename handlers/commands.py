1b8c6cb820aa807fe6dd457f2205cf148d97ac0f
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-27 19:17:33 +0300

    Обработка кнопок 'Авторизация', 'Тесты' и 'Помощь' в commands.py

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from aiogram.filters import Command
+from .keyboards import get_main_keyboard
 
 #/start
 async def start_command(message: types.Message):
@@ -23,3 +24,15 @@ async def help_command(message: types.Message):
 <i>/help</i> - Получить список команд
 """
     await message.answer(help_text, parse_mode="HTML")
+
+#button "Авторизация"
+async def auth_command(message: types.Message):
+    await message.answer("Функция авторизации пока не реализована.")
+
+#button "Тесты"
+async def tests_command(message: types.Message):
+    await message.answer("Функция тестов пока не реализована.")
+
+#button "Помощь"
+async def help_button_command(message: types.Message):
+    await help_command(message)
