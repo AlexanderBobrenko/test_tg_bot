2e0f34c523ef166ca2cbceb8f730308d3384d79e
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-28 16:21:10 +0300

    main.py: при вводе неизвестной команды или сообщения выводится ошибка

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from config import TOKEN
 from handlers.commands import start_command, login_command, help_command, auth_button, tests_button, help_button_command
@@ -15,6 +15,17 @@ dp.message.register(help_command, Command(commands=['help']))
 dp.message.register(auth_button, lambda message: message.text == 'Авторизация')
 dp.message.register(tests_button, lambda message: message.text == 'Тесты')
 dp.message.register(help_button_command, lambda message: message.text == 'Помощь')
+
+async def unknown_command(message: types.Message):
+    if message.text.startswith('/'):
+        await message.answer("Такая команда недоступна.")
+
+dp.message.register(unknown_command, F.text.startswith('/'))
+
+async def unknown_message(message: types.Message):
+    await message.answer("Сообщение не распознано. Используйте команду из списка /help или же воспользуйтесь клавиатурой.")
+
+dp.message.register(unknown_message, ~F.text.in_({'Авторизация', 'Тесты', 'Помощь'}))
 
 async def main():
     try:
