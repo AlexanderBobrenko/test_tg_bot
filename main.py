d891720aa91189873a12455096f690815f434c70
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-30 23:22:36 +0300

    main.py: добавлена команда /logout, изменен импорт

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from .config import TOKEN
-from .handlers.commands import start_command, login_command, help_command, auth_button, tests_button, help_button_command
+from .handlers.commands import start_command, login_command, logout_command, help_command, auth_button, tests_button, help_button_command
 from .handlers.keyboards import get_main_keyboard
 
 bot = Bot(token=TOKEN)
@@ -10,6 +10,7 @@ dp = Dispatcher()
 
 dp.message.register(start_command, Command(commands=['start']))
 dp.message.register(login_command, Command(commands=['login']))
+dp.message.register(logout_command, Command(commands=['logout']))
 dp.message.register(help_command, Command(commands=['help']))
 
 dp.message.register(auth_button, lambda message: message.text == 'Авторизация')
