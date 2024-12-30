dc20e51e9d6352188adb29eda012c03e1b335042
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2024-12-30 22:53:02 +0300

    redis_service.py: изменена кодировка на utf-8

--- services/redis_service.py
+++ services/redis_service.py
@@ -3,16 +3,18 @@ import redis
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 def set_user_status(chat_id, status):
-    redis_client.set(f'user:{chat_id}:status', status)
+    redis_client.set(f'user:{chat_id}:status', status.encode('utf-8'))
 
 def get_user_status(chat_id):
-    return redis_client.get(f'user:{chat_id}:status')
+    status = redis_client.get(f'user:{chat_id}:status')
+    return status.decode('utf-8') if status else None
 
 def set_user_token(chat_id, token):
-    redis_client.set(f'user:{chat_id}:token', token)
+    redis_client.set(f'user:{chat_id}:token', token.encode('utf-8'))
 
 def get_user_token(chat_id):
-    return redis_client.get(f'user:{chat_id}:token')
+    token = redis_client.get(f'user:{chat_id}:token')
+    return token.decode('utf-8') if token else None
 
 def delete_user_session(chat_id):
     redis_client.delete(f'user:{chat_id}:status')
