55561d3e919e475c076e582cea25a420f4e56a21
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2025-01-02 17:29:57 +0300

    keyboards.py: добавлена клавиатура для разнных статусов пользователей (авторизованный, неизвестный)

--- handlers/keyboards.py
+++ handlers/keyboards.py
@@ -1,13 +1,17 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def get_main_keyboard():
-    kb = ReplyKeyboardMarkup(
-        keyboard=[
+def get_main_keyboard(is_authorized: bool = False):
+    if is_authorized:
+        keyboard = [
+            [KeyboardButton(text="Профиль")],
+            [KeyboardButton(text="Тесты")],
+            [KeyboardButton(text="Помощь")]
+        ]
+    else:
+        keyboard = [
             [KeyboardButton(text="Авторизация")],
             [KeyboardButton(text="Тесты")],
             [KeyboardButton(text="Помощь")]
-        ],
-        resize_keyboard=True,
-        one_time_keyboard=False
-    )
-    return kb
+        ]
+    print(f"Keyboard generated (is_authorized={is_authorized}): {keyboard}")
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=False)
