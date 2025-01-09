ac50a2614880229c2e0cd7a8746255c5d5055bb1
sanqe <122402670+AlexanderBobrenko@users.noreply.github.com>
2025-01-09 18:47:11 +0300

    main.py: добавлены команды из commands.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import asyncio
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from .config import TOKEN
-from .handlers.commands import start_command, login_command, logout_command, help_command, auth_button, tests_button, help_button_command
+from .handlers.commands import (
+    start_command, login_command, logout_command, list_tests,
+    test_details_command, create_attempt_command, submit_answer_command,
+    get_results_command, help_command, auth_button, tests_button, help_button_command
+)
 from .handlers.keyboards import get_main_keyboard
 
 bot = Bot(token=TOKEN)
@@ -12,6 +16,11 @@ dp.message.register(start_command, Command(commands=['start']))
 dp.message.register(login_command, Command(commands=['login']))
 dp.message.register(logout_command, Command(commands=['logout']))
 dp.message.register(help_command, Command(commands=['help']))
+dp.message.register(list_tests, Command(commands=['show_tests']))
+dp.message.register(test_details_command, Command(commands=['test_details']))
+dp.message.register(create_attempt_command, Command(commands=['create_attempt']))
+dp.message.register(submit_answer_command, Command(commands=['submit_answer']))
+dp.message.register(get_results_command, Command(commands=['get_results']))
 
 dp.message.register(auth_button, lambda message: message.text == 'Авторизация')
 dp.message.register(tests_button, lambda message: message.text == 'Тесты')
